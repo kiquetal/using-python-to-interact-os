5ec5d5500c58d915113608bf442d3b020c67333f
Enrique M. Talavera <kiquetal@gmail.com>
2022-04-10 03:20:40 -0400

    fuzzy

--- week3/quizz.py
+++ week3/quizz.py
@@ -1,6 +1,8 @@
 The check_web_address function checks if the text passed qualifies as a top-level web address, meaning
-that it contains alphanumeric characters (which includes letters, numbers, and underscores), as well as periods, dashes,
-and a plus sign, followed by a period and a character-only top-level domain such as ".com", ".info", ".edu", etc. Fill in the
+that it contains alphanumeric characters (which includes letters, numbers, and
+underscores), as well as periods, dashes,
+and a plus sign, followed by a period and a character-only top-level domain such as
+".com", ".info", ".edu", etc. Fill in the
 regular expression to do that, using escape characters, wildcards, repetition
 qualifiers, beginning and end-of-line characters, and character classes.
 def check_web_address(text):
